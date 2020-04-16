2c39e6c0714015937e3767d9bf0f26d972ccd481
syth0le <chdan565@gamil.com>
2020-04-16 21:16:58 +0300

    adding origins

---
+++ Python/origins.py
@@ -0,0 +1,29 @@
+fathersDict = {'Harold Abelson': 'Logo',
+               'Andrei Alexandrescu': 'C++',
+               'Alfred Vaino Aho': 'AWK',
+               'Guido van Rossum': 'Python',
+               'Jeremy Ashkenas': 'CoffeeScript',
+               'Walter Bright': 'D',
+               'John George Kemeny': 'Basic',
+               'Peter Naur': 'Algol',
+               'Don Syme': 'F',
+               'Kenneth Eugene Iverson': 'APL',
+               }
+print("Choose mode type.")
+print("Enter 'search' or 'sort': ", end='')
+choose = input()
+print()
+
+if choose == 'sort':
+    sortList = sorted(fathersDict.keys())
+    for name in sortList:
+        print(name, " - ", fathersDict[name])
+elif choose == 'search':
+    print("Enter developer's name: ", end='')
+    name = input()
+    for father in fathersDict.keys():
+        if father == name:
+            print(fathersDict[name], "was created by", name)
+else:
+    print("Incorrect mode type. Try again")
+
